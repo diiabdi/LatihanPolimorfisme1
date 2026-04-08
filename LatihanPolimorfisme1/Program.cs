fc29f45a6be96fe8239657e1d2a07ddb29a39542
diiabdi <manabdi078@gmail.com>
2026-04-08 14:49:34 +0700

    Polimorfismeperubahan

--- LatihanPolimorfisme1/Program.cs
+++ LatihanPolimorfisme1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿imageProcecsor imageProcecsor = new imageProcecsor();
+
+imageProcecsor.Reize(-20);
+imageProcecsor.Reize(80);
+imageProcecsor.Reize(150);
+imageProcecsor.Reize(100);
+
+imageProcecsor.Reize(124, 234);
+imageProcecsor.Reize("A4");
+class imageProcecsor
+{
+    public void Reize(int percentage)     
+        {
+        if (percentage <= 0)
+        {
+            Console.WriteLine("Persentase tidak boleh nol atau negatif"); 
+        }
+        else if (percentage < 100)
+        {
+            Console.WriteLine($"Mengecilkan gambar menjadi {percentage}");
+        }
+        else if (percentage > 100)
+        {
+            Console.WriteLine($"Memperbesar gambar menjadi {percentage}");
+        }
+        else
+        {
+             Console.WriteLine("Ukuran gambar tetap");
+        }
+    }
+    public void Reize(int lebar , int tinggi)
+    {
+        Console.WriteLine($"Mengubah Ukuran Gambar menjadi px {lebar}x{tinggi}");
+    }
+    public void Reize(string ukuran_kertas)
+    {
+        Console.WriteLine($"Menyesuaikan Ke format kertas {ukuran_kertas}");
+    }
+    
+}
